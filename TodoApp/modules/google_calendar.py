3589943d2bea9de9906758dfe68fb2c43f86b62f
name goes here <email goes here>
2025-08-01 10:30:58 +0900

    Create credentials.json

--- TodoApp/modules/google_calendar.py
+++ TodoApp/modules/google_calendar.py
@@ -26,7 +26,7 @@ def google_calendar():
         if creds and creds.expired and creds.refresh_token:
             creds.refresh(Request())
         else:
-            flow = InstalledAppFlow.from_client_secrets_file('credentials.json', SCOPES)
+            flow = InstalledAppFlow.from_client_secrets_file('./credentials.json', SCOPES)
             creds = flow.run_local_server(port=0)
 
         # 토큰 저장
